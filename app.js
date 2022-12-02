eccb90f60271df6c061fd7cf1dc5421946bbb33b
OubellilFouad <oubellilfouad4@gmail.com>
2022-12-02 21:03:10 +0100

    Linked the js file and created a function that gets the computer output

---
+++ app.js
@@ -0,0 +1,10 @@
+function getComputerChoice(){
+    let options = ["Rock","Paper","Scissors"];
+    let comChoice = options[Math.floor(Math.random()*options.length)];
+    console.log(comChoice);
+    
+}
+function playRound(){
+
+}
+getComputerChoice();
